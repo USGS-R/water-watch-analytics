f4438b366f3da7b15aa0fa234b5dae4eb371b889
wdwatkins <wwatkins@usgs.gov>
2018-04-17 15:50:44 -0500

    forgot to functionalize

--- R/water_watch_urls.R
+++ R/water_watch_urls.R
@@ -21,23 +21,24 @@ path_df_no_string <- path_df %>% filter(grepl(pattern = "wqwatch", x = pagePath,
   return(path_df_human_names)
 }
 
-#assign human names based on rules from Google sheet
-path_df_human <- path_df %>% filter(!grepl(pattern = "wqwatch", x = pagePath, 
-                                      ignore.case = TRUE)) %>% 
-  mutate(pagePath = gsub(pattern = "index.php", replacement = "", x = pagePath),
-                   contents = ifelse(pagePath == "/", yes = "Root home page",
-                                     no = "Everything else")) %>% 
-  mutate(contents = ifelse(test = (pagePath == "/?id=ww_current" | pagePath == "/?id=real"),
-                           yes = "National realtime historical percentile map", no = contents)) %>% 
-  mutate(contents = ifelse(test = grepl("id=ww_flood", x = pagePath) & !grepl("r=..", x= pagePath),
-         yes = "National flood and high flow map", no = contents)) %>% 
-  mutate(contents = ifelse(test = grepl("id=ww_flood", x = pagePath) & grepl("r=..", x= pagePath),
-         yes = "State/region flood and high flow maps", no = contents)) %>%
-  mutate(contents = ifelse(test = grepl("m=real", x = pagePath) & grepl("r=..", x= pagePath),
-         yes = "State/region real time \nhistorical percentile maps", no = contents)) %>% 
-  group_by(contents) %>% summarize(uniquePageViews = sum(uniquePageViews))
-wwatch_plot <- ggplot(path_df_human, aes(x = reorder(contents, -uniquePageViews), y = uniquePageViews))+
-  geom_col() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(x = "Page group", y = "Summed unique page views") + scale_y_continuous(labels=scales::comma)
-ggsave(filename = "wwatch_paths.png")
-
+analyze_wwatch_urls <- function(path_df) {
+  #assign human names based on rules from Google sheet
+  path_df_human <- path_df %>% filter(!grepl(pattern = "wqwatch", x = pagePath, 
+                                             ignore.case = TRUE)) %>% 
+    mutate(pagePath = gsub(pattern = "index.php", replacement = "", x = pagePath),
+           contents = ifelse(pagePath == "/", yes = "Root home page",
+                             no = "Everything else")) %>% 
+    mutate(contents = ifelse(test = (pagePath == "/?id=ww_current" | pagePath == "/?id=real"),
+                             yes = "National realtime historical percentile map", no = contents)) %>% 
+    mutate(contents = ifelse(test = grepl("id=ww_flood", x = pagePath) & !grepl("r=..", x= pagePath),
+                             yes = "National flood and high flow map", no = contents)) %>% 
+    mutate(contents = ifelse(test = grepl("id=ww_flood", x = pagePath) & grepl("r=..", x= pagePath),
+                             yes = "State/region flood and high flow maps", no = contents)) %>%
+    mutate(contents = ifelse(test = grepl("m=real", x = pagePath) & grepl("r=..", x= pagePath),
+                             yes = "State/region real time \nhistorical percentile maps", no = contents)) %>% 
+    group_by(contents) %>% summarize(uniquePageViews = sum(uniquePageViews))
+  wwatch_plot <- ggplot(path_df_human, aes(x = reorder(contents, -uniquePageViews), y = uniquePageViews))+
+    geom_col() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(x = "Page group", y = "Summed unique page views") + scale_y_continuous(labels=scales::comma)
+  ggsave(filename = "wwatch_paths.png")
+}
