8d66e9728c070c9c43a99a193d79934a8fd973ac
wdwatkins <wwatkins@usgs.gov>
2018-04-20 15:53:29 -0500

    try out multiple assignment with zeallot

--- R/advanced.R
+++ R/advanced.R
@@ -1,5 +1,6 @@
 library(googleAnalyticsR)
 library(googleAuthR)
+library(zeallot)
 
 config <- yaml::read_yaml('config.yaml')
 wqwatch_segment <- readRDS('wqwatch_segment.rds')
@@ -19,19 +20,22 @@ nwis_web <- google_analytics(viewId = config$ga_views$nwisweb_desktop,
                              date_range = date_range, metrics = "uniquePageviews",
                              dimensions = c("source", "pagePath", "deviceCategory"), 
                              max = -1)
+
 nwis_web_filtered <- nwis_web %>% filter(grepl(pattern = "site_no", x = pagePath) &
                                            grepl(patter="waterwatch", x= source))
+attr(nwis_web_filtered, "dataPullDate") <- Sys.Date()
 all_watch_pages$all_raw$nwis <- nwis_web_filtered
 
 saveRDS(all_watch_pages, file = 'all_pages.rds')
 
 #page group breakdowns
+source('R/plot_helper.R')
 source('R/gwwatch_urls.R')
 gwwatch_urls <- analyze_gww_urls(path_df = all_watch_pages$all_raw$gwwatch) %>% 
   mutate(watch = "GroundwaterWatch")
 
 source('R/water_watch_urls.R')
-wwatch_urls <- analyze_wwatch_urls(path_df = all_watch_pages$all_raw$wwatch, 
+c(df, plot) %<-% analyze_wwatch_urls(path_df = all_watch_pages$all_raw$wwatch, 
                                    nwis_df = all_watch_pages$all_raw$nwis) %>% 
   mutate(watch = "WaterWatch")
 wqwatch_urls <- analyze_wqwatch_urls(all_watch_pages$all_raw$wqwatch) %>% 
